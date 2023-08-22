a57863ff1e81a61031b399282abb23b43eab454f
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-08-23 00:08:35 +0800

    Update RPS.c

--- LEVEL_2/GNU/RPS.c
+++ LEVEL_2/GNU/RPS.c
@@ -1,11 +1,4 @@
-//WELCOME_codename::FRANKLIN
-#include "frank_talk.h"
-#include "gameplay.h"
-//declare
-int frank,player,player_hp,frank_hp,strike_frnk,strike_plyr,effect;
-
-//main
-int main(){
+  GNU nano 7.2                                                                                                                                                RPS.c                                                                                                                                                          
 //initialize
  frank_hp=50;
  player_hp=50;
@@ -29,7 +22,7 @@ while(1){
  fprintf(stdout,"\033[H\033[2J");//ansi_code_to_clear_stdout
  fflush(stdout);
 //reaction
- if( hitpoint(&frank_hp,&player_hp,hit(frank,player),&strike_frnk,&strike_plyr,&effect)==-1 )break;
+ if( hitpoint(&frank_hp,&player_hp,hit(frank,player),&strike_frnk,&strike_plyr,&effect,&score)==-1 )break;
  game_effect(effect);
  frank_react(frank,player);
  fprintf(stdout,"[YOU]%d::%d::[FRANK]%d::%d\n\n",player_hp,strike_plyr,frank_hp,strike_frnk);
@@ -39,6 +32,8 @@ while(1){
 }
  if(player_hp<=0)fprintf(stdout,"----GAME OVER----\n\e[0;91m <<<YOU LOSE>>>\n");
  if(frank_hp<=0)fprintf(stdout," ----GAME OVER----\n\e[0;94m<<<!!!YOU WIN!!!>>>\n");
+ //printf("%d\n",score);
+ scoreboard(&score,file);
 //esc
  return 0;
 }
