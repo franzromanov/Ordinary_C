f3f8c8b39779ead507e15d468c560a1c83f23b19
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-08-23 00:11:25 +0800

    Add files via upload

--- LEVEL_2/GNU/gameplay.h
+++ LEVEL_2/GNU/gameplay.h
@@ -4,7 +4,10 @@
 int fight(int* frank,int* player);
 
 //hit_point_Calculation
-int hitpoint(int* frank_hp,int* player_hp,int hit,int* strike_frnk,int* strike_plyr,int* effect);
+int hitpoint(int* frank_hp,int* player_hp,int hit,int* strike_frnk,int* strike_plyr,int* effect,int* score);
 
 //game_effect
 void game_effect(int effect);
+
+//score_board_View
+void scoreboard(int *score, FILE* file);
